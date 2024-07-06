279c782d4dbc9ee66d5a62d22c05c56dee1e02c5
mohit-soni2003 <mohitsonip1847@gmail.com>
2024-07-06 15:56:35 +0530

    The environment of Backend i.e node , express module setup . In frontend React setup is done.

---
+++ frontend/blog-website/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Nav from "./components/Nav"
+
+function App() {
+
+  return (
+    <div className="app">
+      <Nav></Nav>
+      THis is react app</div>
+  )
+}
+
+export default App
---
+++ frontend/blog-website/src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Nav.css"
+
+export default function Nav() {
+  return (
+    <div className='nav-bar'>
+        THis is navigation
+        <h1>write nav</h1>
+    </div>
+  )
+}
