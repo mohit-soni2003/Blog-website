4167745a650b4fe173e71e3d30be69f98889f44f
HARSHILd11 <dhoot.harshil@gmail.com>
2024-07-09 01:51:38 +0530

    sign in

--- frontend/blog-website/src/App.jsx
+++ frontend/blog-website/src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import './App.css'
 
 import Header from "./components/Nav"
+import SignIn from "./components/Sign-in-form"
 function App() {
 
   return (
     <div className="app">
    <Header></Header>
+   <SignIn></SignIn>
      </div>
       
       
