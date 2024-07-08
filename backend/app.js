73ebd550471ac2b522f8b9c9caa3921c0325b65f
mohit-soni2003 <mohitsonip1847@gmail.com>
2024-07-09 01:20:33 +0530

    Signup and SignIn API functionality added

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,17 @@
+const router = require("./routes/auth")
 const express = require("express")
 const app = express();
 const port = 8080;
-const router = express.Router() 
+const mongoose = require("mongoose")
+const url = "mongodb+srv://2mohitsoni:Mohit1234@cluster0.wjfspkb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+
+mongoose.connect(url)
+mongoose.connection.on('connected', () => console.log('connected'));
+mongoose.connection.on("error",()=>{console.log("Not Connected To Database......")})
+
+app.use(express.json())  // parse the json data comming from post request 
+app.use(router)
 
 app.get("/",(req,res)=>{
     res.send("hello Server is ready")
---
+++ backend/routes/auth.js
@@ -0,0 +1,86 @@
+const express = require("express")
+const router = express.Router()
+const USERS = require("../models/users")
+const bcrypt = require('bcrypt');
+
+
+
+
+
+
+// Signup  Route ----------- ------------ ----------- ------------ ------------ ----------
+
+router.get("/signup",(req,res)=>{
+    res.json({message:"signup page get request"})
+})
+
+router.post("/signup",(req,res)=>{
+    const {name , email , username , password} =req.body;
+    // check all fields are entered ........
+    if(!name , !email , !username , !password ){
+        return res.json({error : "Please add all the fields"})
+    }
+    // check either user with same email or username should not registered .....
+    USERS.findOne({$or : [{email:email}, {username : username}]})
+    .then((savedUser)=>{
+        if(savedUser){
+            return res.json({message : "user with same username or email exists"})
+        }
+
+        bcrypt.hash(password,10).then((hashedPassword)=>{
+            const user = new USERS({
+                name , 
+                email,
+                username,
+                password : hashedPassword,
+            })
+        
+            user.save()
+            .then((res)=>{
+                console.log(res)
+            })
+            .catch(err=>console.log(err))
+            res.json({message:"signup post"})
+            
+        })
+
+       
+
+    })
+
+   
+})
+
+// Signin get Route ----------- ------------ ----------- ------------ ------------ ----------
+
+router.get("/signin",(req,res)=>{
+    res.json({message:"signin"})
+})
+
+router.post("/signin",(req,res)=>{
+    const {email , password} = req.body;
+
+    if(!email , !password){
+        return res.status(422).json({error : "Please Add Email and Password"})
+    }
+    
+    USERS.findOne({email:email})
+    .then((savedUser)=>{
+        if(!savedUser){
+            return res.status(422).json({error : "Invalid Email"})
+        }
+        bcrypt.compare(password,savedUser.password)
+        .then((match)=>{
+            if(match){
+                return res.status(422).json({message : "Signin Successful"})
+            }
+            else{
+                return res.status(422).json({error : "Invalid Password"})
+            }
+        })
+        .catch(err=>console.log(err))
+    })
+
+})
+
+ module.exports=router
