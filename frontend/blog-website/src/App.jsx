222050ae4234404fdbad232a0c8148f751a76a1c
HARSHILd11 <dhoot.harshil@gmail.com>
2024-07-09 12:18:01 +0530

    Update App.jsx
    Co-Authored-By: mohit-soni2003 <155765049+mohit-soni2003@users.noreply.github.com>

--- frontend/blog-website/src/App.jsx
+++ frontend/blog-website/src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import './App.css'
 
 import Header from "./components/Nav"
-import SignIn from "./components/Sign-up-form"
+
 function App() {
 
   return (
     <div className="app">
    <Header></Header>
-   <SignIn></SignIn>
+  
      </div>
       
       
