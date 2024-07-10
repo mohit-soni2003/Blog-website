a451b8c423f96fe3533eca9adab4cf33b4aa51d6
HARSHILd11 <dhoot.harshil@gmail.com>
2024-07-11 02:00:22 +0530

    edited Create Blog Form
    edited Create Blog Form
    
    Co-Authored-By: mohit-soni2003 <155765049+mohit-soni2003@users.noreply.github.com>

--- frontend/blog-website/src/components/CreateBlog.jsx
+++ frontend/blog-website/src/components/CreateBlog.jsx
@@ -1,27 +1,43 @@
 import Form from 'react-bootstrap/Form';
-
+import './CreateBlog.css';
+import NavigationBar from "./Nav"
 function CreateBlog() {
-  return (
-    <Form className='m-5'>
-    <Form.Select aria-label="Default select example">
-      <option className='text-center'>Select Category </option>
-      <option value="fashion">Fashion & Beauty</option>
-      <option value="food">Food</option>
-      <option value="sports">Sports Fitness & Wellness</option>
-      <option value="travel">Travel</option>
-      <option value="entertainment">Entertainment</option>
-      <option value="Technology">Technology</option>
-    </Form.Select>
-    <Form.Group className="mb-3" controlId="Description">
-        <Form.Label>Description</Form.Label>
-        <Form.Control type="text" placeholder="Enter your Blog Description"  />
-      </Form.Group>
-    
-    
-            
-    </Form>
-    
-  );
+    return (
+
+        <>
+            <NavigationBar></NavigationBar>
+
+            <div className='form'>
+                <Form className='m-4 inputfield'>
+                    <Form.Select aria-label="Default select example">
+                        <option className='text-center'>Select Category </option>
+                        <option value="fashion">Fashion & Beauty</option>
+                        <option value="food">Food</option>
+                        <option value="sports">Sports Fitness & Wellness</option>
+                        <option value="travel">Travel</option>
+                        <option value="entertainment">Entertainment</option>
+                        <option value="Technology">Technology</option>
+                    </Form.Select>
+
+                    <Form.Group className="mb-3 inputfield" controlId="Description">
+                        <Form.Label>Description</Form.Label>
+                        <Form.Control type="text" placeholder="Enter A small description " />
+                    </Form.Group>
+                    <Form.Group className="mb-3 inputfield" controlId="exampleForm.ControlTextarea1">
+                        <Form.Label>Write your Blog here</Form.Label>
+                        <Form.Control as="textarea" rows={3} placeholder='Write your Blog Here . . . . .' />
+                    </Form.Group>
+                    <Form.Group className="mb-3 inputfield" controlId="Description">
+                        <Form.Label>Image Link</Form.Label>
+                        <Form.Control type="link" placeholder="Paste Image link here" />
+                    </Form.Group>
+
+                    <input type="button" value={"POST"} />
+
+                </Form>
+            </div></>
+
+    );
 }
 
 export default CreateBlog;
