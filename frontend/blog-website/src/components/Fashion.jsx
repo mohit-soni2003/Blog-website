ab26ebe28f8a9958a6cfe4c8271ad6ec390abef0
HARSHILd11 <dhoot.harshil@gmail.com>
2024-07-12 00:12:55 +0530

    Update Fashion.jsx

--- frontend/blog-website/src/components/Fashion.jsx
+++ frontend/blog-website/src/components/Fashion.jsx
@@ -5,10 +5,29 @@ import NavigationBar from "./Nav"
 
 
 function Fashion() {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+      const fetchData = async () => {
+        const response = await fetch("http://localhost:8080/categories/fashion");
+        const data = await response.json();
+        setData(data);
+      };
+  
+      fetchData();
+    }, []);
+
+
+
+
+    console.log(data);
     return (
         <>
             <NavigationBar></NavigationBar>
-            <h1>Hello</h1></>
+            <h1>Hello</h1>
+           
+            
+            </>
     )
 
 
