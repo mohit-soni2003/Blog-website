175cc095a42ec1a4fcb520ab80310aa1d79e90d4
HARSHILd11 <dhoot.harshil@gmail.com>
2024-07-16 17:03:03 +0530

    few changes

--- frontend/blog-website/src/components/HomeC1.jsx
+++ frontend/blog-website/src/components/HomeC1.jsx
@@ -1,18 +1,47 @@
-import "./HomeC1.css"
-import mainpage from "../img/mainpage1.jpg"
+import React, { useState, useEffect } from 'react';
+import './HomeC1.css';
+import mainpage from '../img/mainpage1.jpg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
+
 function HomeC1() {
-    return (
-        <>
-            <div className="home-container">
+  const [typedText, setTypedText] = useState(''); // Manage typed text state
 
-                <div className="slogan-part1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, corrupti! <br /><Button variant="primary" className="Create-blog">Create Blog</Button> <Button variant="primary" className="Explore-blog">Explore Blog</Button></div>
+  useEffect(() => {
+    const text = "Write, rewrite, and publish! Let your voice be heard.";
+    const speed = 100;
 
-                <div className="slogan-part2"><img src={mainpage} alt="" /></div>
-            </div>
-        </>
-    )
+    let i = 0;
+    const typeWriter = () => {
+      if (i < text.length) {
+        setTypedText(typedText + text.charAt(i));
+        i++;
+        setTimeout(typeWriter, speed);
+      }
+    };
+
+
+  }, []); 
+
+  return (
+    <>
+      <div className="home-container">
+        <div className="slogan-part1">
+          <h1 className="slogan">Write, rewrite, and publish! Let your voice be heard."</h1>
+          <br />
+          <Button variant="primary" className="Create-blog">
+            Create Blog
+          </Button>
+          <Button variant="primary" className="Explore-blog">
+            Explore Blog
+          </Button>
+        </div>
+        <div className="slogan-part2">
+          <img src={mainpage} alt="" />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default HomeC1;
