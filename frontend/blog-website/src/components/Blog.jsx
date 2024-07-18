0691d9f052c0c4a93d88b5611d8215c44893d00a
mohit-soni2003 <mohitsonip1847@gmail.com>
2024-07-19 00:18:08 +0530

    view Blog component styling

--- frontend/blog-website/src/components/Blog.jsx
+++ frontend/blog-website/src/components/Blog.jsx
@@ -1,21 +1,23 @@
 import { useLocation } from 'react-router-dom';
+import "./Blog.css"
 
 const Blog = () => {
   const location = useLocation();
   const { blogData } = location.state || {};
-
+  console.log(blogData)
   return (
-    <div>
+    <div className='full-blog-container'>
       {blogData ? (
-        <div>
-          <h1>{blogData.title}</h1>
-          <h1>{blogData.description}</h1>
-          <h1>{blogData.categories}</h1>
-          <h1>{blogData._id}</h1>
-          <img src={blogData.image} alt="" />
-          <h2>{blogData.content}</h2>
+        <div className='blog'>
+          <div className="blog-title">{blogData.title}</div>
+          <div className="blog-image"><img src={blogData.image} alt="" /></div>
+          <div className="blog-description">{blogData.description}</div>
+          <div className="blog-content">{blogData.content}</div>
+          <h5>{blogData.categories}</h5>
+          <div className="created-by">Created by : {blogData.author.name}Mohit Soni</div>
+
           
-          {/* Render other blog data as needed */}
+        
         </div>
       ) : (
         <p>No blog data available.</p>
