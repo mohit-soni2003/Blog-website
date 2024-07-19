d4a7ad0000f97a469bf380e592050822f6c9b268
HARSHILd11 <dhoot.harshil@gmail.com>
2024-07-19 20:23:24 +0530

    Update Fashion.jsx

--- frontend/blog-website/src/components/Fashion.jsx
+++ frontend/blog-website/src/components/Fashion.jsx
@@ -62,7 +62,6 @@ function Fashion() {
     <>
       <div className="category-blog-container">
 
-
       {/*cards*/}
       {data.map((blogs) => {
         console.log(blogs)
@@ -79,25 +78,6 @@ function Fashion() {
               </Card.Body>
             </Card>
           </div>
-
-        {/*cards*/}
-        {data.map((blogs) => {
-          console.log(blogs)
-          return (
-            <div id={blogs._id} onClick={() => visitBlog(blogs._id)}>
-              <Card className="category-card">
-                <div className="cat-image">
-                  <Card.Img variant="top" src={blogs.image} />
-                </div>
-                <Card.Body >
-                  <Card.Title>{blogs.title}</Card.Title>
-                  <Card.Text className="cat-card-desc">
-                    {blogs.description}
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </div>
-
 
 
           )
